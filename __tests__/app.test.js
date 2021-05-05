b14cb873225ef1de0273b80b354bacd18d1ac1b4
Tucker Hoog <tuckerhoog@tutanota.com>
2021-05-05 18:32:05 -0400

    post pass test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,110 +7,79 @@ const request = supertest(app);
 
 describe('API Routes', () => {
 
+  // beforeAll(() => {
+  //   execSync('npm run setup-db');
+  // });
+
   beforeAll(() => {
-    execSync('npm run setup-db');
+    execSync('npm run recreate-tables');
   });
 
   afterAll(async () => {
     return client.end();
   });
 
-  const expectedBooks = [
-    {
-      id: expect.any(Number),
-      title: 'The Birth of Tragedy',
-      genre: 'philosophy as lit',
-      url: 'covers/BoT.jpg',
-      year: 1872,
-      pages: 160,
-      wasPublished: true
-    },
-    {
-      id: expect.any(Number),
-      title: 'On Truth and Lies in a Nonmoral Sense',
-      genre: 'philosophy as lit',
-      url: 'covers/TaL.jpg',
-      year: 1873,
-      pages: 30,
-      wasPublished: false
-    },
-    {
-      id: expect.any(Number),
-      title: 'Human, All too Human',
-      genre: 'philosophy as lit',
-      url: 'covers/HalT.webp',
-      year: 1878,
-      pages: 188,
-      wasPublished: true
-    },
-    {
-      id: expect.any(Number),
-      title: 'The Gay Science',
-      genre: 'philosophy as lit',
-      url: 'covers/tGS.jpg',
-      year: 1882,
-      pages: 296,
-      wasPublished: true
-    },
-    {
-      id: expect.any(Number),
-      title: 'Thus Spake Zarathustra',
-      genre: 'philosophy as lit',
-      url: 'covers/thus.jpg',
-      year: 1883,
-      pages: 352,
-      wasPublished: true
-    },
-    {
-      id: expect.any(Number),
-      title: 'Beyond Good and Evil',
-      genre: 'philosophy as lit',
-      url: 'covers/BGE.jpeg',
-      year: 1886,
-      pages: 288,
-      wasPublished: true
-    },
-    {
-      id: expect.any(Number),
-      title: 'On The Geneology of Morality',
-      genre: 'philosophy as lit',
-      url: 'covers/GM.jpg',
-      year: 1887,
-      pages: 177,
-      wasPublished: true
-    },
-    {
-      id: expect.any(Number),
-      title: 'Twilight of the Idols',
-      genre: 'philosophy as lit',
-      url: 'covers/TI.jpg',
-      year: 1888,
-      pages: 208,
-      wasPublished: true
-    }
-  ];
+  let H2H = {
+    id: expect.any(Number),
+    title: 'Human, All too Human',
+    genre: 'philosophy as lit',
+    url: 'covers/HalT.webp',
+    year: 1878,
+    pages: 188,
+    wasPublished: true
+  };
 
-  // If a GET request is made to /api/cats, does:
-  // 1) the server respond with status of 200
-  // 2) the body match the expected API data?
-  it('GET /api/books', async () => {
+  let BGE = {
+    id: expect.any(Number),
+    title: 'Beyond Good and Evil',
+    genre: 'philosophy as lit',
+    url: 'covers/BGE.jpeg',
+    year: 1886,
+    pages: 288,
+    wasPublished: true
+  };
+
+  let morals = {
+    id: expect.any(Number),
+    title: 'On The Geneology of Morality',
+    genre: 'philosophy as lit',
+    url: 'covers/GM.jpg',
+    year: 1887,
+    pages: 177,
+    wasPublished: true
+  };
+
+  let idols = {
+    id: expect.any(Number),
+    title: 'Twilight of the Idols',
+    genre: 'philosophy as lit',
+    url: 'covers/TI.jpg',
+    year: 1888,
+    pages: 208,
+    wasPublished: true
+  };
+
+  it.only('POST H2H to /api/books', async () => {
+    const response = await request
+      .post('/api/books')
+      .send(H2H);
+    console.log(response.text);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(H2H);
+
+    H2H = response.body;
+  });
+
+
+  it('POST /api/books/H2H', async () => {
     // act - make the request
-    const response = await request.get('/api/books');
+    const response = await request.post('/api/books/H2H');
 
     // was response OK (200)?
     expect(response.status).toBe(200);
 
     // did it return the data we expected?
-    expect(response.body).toEqual(expectedBooks);
+    expect(response.body).toEqual(H2H);
 
-  });
-
-  // If a GET request is made to /api/cats/:id, does:
-  // 1) the server respond with status of 200
-  // 2) the body match the expected API data for the cat with that id?
-  test('GET /api/books/:id', async () => {
-    const response = await request.get('/api/books/3');
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual(expectedBooks[2]);
   });
 });
